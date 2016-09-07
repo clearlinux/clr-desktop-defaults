01fc6867332c4da26dd5a37553edd49e3c2734e5
Ikey Doherty <michael.i.doherty@intel.com>
2016-09-07 16:22:29 +0100

    Initial commit of clr-desktop-defaults
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+clr-desktop-defaults
+=====================
+
+Provides default configuration and startup assistance for the desktop experience within
+the `os-utils-gui` bundle.
+
+
+License
+-------
+
+clr-desktop-defaults is available under the terms of the `MIT` license.
+
+Copyright © 2016 Intel Corporation
---
+++ clr-desktop-files.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# 
+# Copyright (c) 2016 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+if [[ -e "$HOME/.clr_configured_desktop" ]]; then
+    exit 0
+fi
+
+declare -A DESKTOP_FILES=(
+    ["xfce4-terminal.desktop"]="/usr/share/applications/xfce4-terminal.desktop"
+)
+
+REQUIRED_DIRS=( "$HOME/Desktop" )
+
+function install_files()
+{
+    local tgtdir="$1"
+    local perm="$2"
+    declare -n files=$3
+
+    for target_path in "${!files[@]}"; do
+        local source_path="${files["${target_path}"]}";
+        if [[ ! -e "${source_path}" ]]; then
+            continue;
+        fi
+        local fpath="${tgtdir}/${target_path}"
+        if [[ ! -e "${fpath}" ]]; then
+            install -D -m "$perm" "${source_path}" "${fpath}"
+        fi
+    done
+}
+
+function install_dirs()
+{
+    declare -n rdirs=$1
+    for i in "${rdirs[@]}"; do
+        if [[ ! -d "${i}" ]]; then
+            install -D -d -m 00755 "${i}"
+        fi
+    done
+}
+
+# Ensure base dirs
+install_dirs REQUIRED_DIRS
+
+# Shortcuts, etc.
+install_files "$HOME/Desktop" 00755 DESKTOP_FILES
+
+echo "1" > "$HOME/.clr_configured_desktop" 
